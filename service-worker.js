0d0060ea3098f1031203021388395fa3f9387aa0
mr-raman-kr <raman.24csz0012@iitrpr.ac.in>
2024-10-08 17:55:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener("push", function (event) {
   const data = event.data.json();
   const options = {
     body: data.body,
-    icon: "images/encodedcoder.png", // Path to your icon
-    badge: "images/encodedcoder.png", // Path to your icon
+    icon: "images/rmn.png", // Path to your icon
+    badge: "images/rmn.png", // Path to your icon
   };
 
   event.waitUntil(self.registration.showNotification(data.title, options));
